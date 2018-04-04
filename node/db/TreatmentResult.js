b0c77de578c45ee5418cd4aeb467d0cf0f322186
Seok-IL <thirty950@naver.com>
2018-04-04 21:00:00 +0900

    진료결과 입력할수 있게 만들어 뒀음.

--- node/db/TreatmentResult.js
+++ node/db/TreatmentResult.js
@@ -10,13 +10,24 @@ app.set('view engine', 'jade');
 app.set('views', './views');
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
-var DBNAME = 'TreatmentResult'
-var TABLENAME = 'treatmentResult'
+var DBNAME = "TreatmentResult"
+var TABLENAME = "treatmentResult"
 
+const db = mysql.createConnection({
+    host: 'localhost',  
+    user: 'root',
+    password: '1q2w3e4r5t',
+    database: DBNAME
+});
 
 app.get('/', function(req, res){
         res.render('home');
+});
+
+app.get('/inputResult', function (req,res){
+    res.render('inputResult');
 });
 
 app.post('/newDatabase',function(req,res){
@@ -30,22 +41,26 @@ app.post('/newDatabase',function(req,res){
 });
 
 app.post('/newTable', function(req, res) {
-    var sql = 'CREATE TABLE ?(patientID int, contents VARCHAR(255), PRIMARY KEY(patientID))';
+    var sql = 'CREATE TABLE ' + TABLENAME + '(patientID int, contents VARCHAR(255), PRIMARY KEY(patientID))';
     db.query(sql,TABLENAME, (err, result) => {
         if (err) throw err;
         console.log(result);
-        res.send('? table created...',TABLENAME);
+        res.send('table' + TABLENAME +   'created...');
     });
 });
 
 app.post('/newTreatmentResult',function(req,res){
-    var sql = 'INSERT INTO ? (patientID, contents) VALUES(? ?)'
-    var params = [req.body.patientID,req.body.contents]
-
-    db.query(sq,params,function(err,result){
+    var sql = 'INSERT INTO ' + TABLENAME + '(patientID, contents) VALUES(?,?)';
+    var params = [req.body.patientID,req.body.contents];
+    console.log('Request : ' + sql);
+    db.query(sql,params, function(err,result){
         if(err) throw err;
         console.log(result);
         res.send('Inserted...');
     });    
 })
 
+
+app.listen('3000', () => {
+    console.log('Server started on port 3000');
+});
