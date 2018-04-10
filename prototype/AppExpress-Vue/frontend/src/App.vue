592aed307bcddfe327ece5c7d87222c857ca8bf0
pmonormal <pmon2648@daum.net>
2018-04-10 20:15:19 +0900

    [proto. express-vue] vue login form 완성

---
+++ prototype/AppExpress-Vue/frontend/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <HoscareHeader></HoscareHeader>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import HoscareHeader from './components/HoscareHeader.vue'
+export default {
+  name: 'App',
+  components: {
+    'HoscareHeader': HoscareHeader,
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ prototype/AppExpress-Vue/frontend/src/components/LoginPage.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+      <p>
+        <input type="text" message="ID" v-model="id"></input>
+      </p>
+      <p>
+        <input type="password" v-model="password"></input>
+      </p>
+      <p>
+        <button v-on:click="sendLogin">Log in</button>
+        <button>Sign up</button>
+      </p>
+
+    </ul>
+  </div>
+</template>
+
+
+<script>
+  export default {
+    data() {
+      return {
+        id: '',
+        password: ''
+      }
+    },
+    methods: {
+      // 로그인 정보 보내기
+      sendLogin() {
+        console.log(this.id, this.password);
+      }
+    }
+  }
+</script>
+
+
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
