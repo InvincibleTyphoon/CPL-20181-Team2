01e7f11982eab1070e6f63fad7850d6ab03d0ad6
Seok-IL <thirty950@naver.com>
2018-04-28 00:08:38 +0900

    백엔드에 mqtt 기본 함수들 추가함

--- backend/app.js
+++ backend/app.js
@@ -6,6 +6,8 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var mqtt = require('mqtt')
+var client = mqtt.connect('mqtt://127.0.0.1');
 
 var app = express();
 
@@ -37,5 +39,32 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+
+// mqtt 관련. 클라이언트 접속시 호출
+// 클라이언트에게 greetings, time 의 topic을 구독하게 함
+// 테스트를 위해서 3초마다 메시지를 publish함
+client.on('connect', function () {
+  client.subscribe('greetings'); 
+  client.subscribe('time');
+  publish("greetings", "Nice to meet you.");
+  programmedPublish();
+});
+
+// mqtt 관련. 3초마다 메시지를 publish
+function programmedPublish() {
+  publish("time", new Date().toISOString());
+  setTimeout(programmedPublish, 3000);
+}
+
+// mqtt 관련. 클라이언트에게 메시지를 보내고 나면 무슨 메시지를 보냈는지 Log로 출력
+client.on('message', function (topic, message) {
+  console.log("Published: " + topic.toString() + ' :: ' + message.toString());
+});
+
+// mqtt 관련. topic의 message를 publish함
+function publish(topic, message) {
+  client.publish(topic, message);
+}
 
 module.exports = app;
