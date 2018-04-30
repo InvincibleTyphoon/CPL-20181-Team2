16cb762c4fd9ede866518275466856216a4a19c4
floidea@github.com <floidea@github.com>
2018-04-30 23:56:33 +0900

    login확인

--- backend/app.js
+++ backend/app.js
@@ -8,6 +8,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var loginRouter = require('./routes/logins');
+var signUpRouter = require('./routes/sign-up');
 
 var app = express();
 
@@ -25,6 +26,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/api/logins', loginRouter);
+app.use('/api/sign-up', signUpRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
