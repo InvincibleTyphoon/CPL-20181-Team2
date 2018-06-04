76c681b887fe1155ab8c5ab1466dbab6ea86b34b
Seok-IL <thirty950@naver.com>
2018-06-05 01:51:58 +0900

    Alarm Page 테스트 용으로 제작. 1초마다 알람을 갱신하여 출력함.

--- frontend/src/pages/MainPages/AlarmPage.vue
+++ frontend/src/pages/MainPages/AlarmPage.vue
@@ -1,34 +1,34 @@
 <template id="AlarmPage">
   <v-ons-page>
 
-  <v-ons-card v-for="item in cardList">
-    <v-ons-row>
-      <v-ons-col width="70%">
-        <v-ons-row>
-            <div class="title" style="font-size: 20px">
-              {{ item.title }}
-            </div>
-            <div class="fontsmall"
-              style="font-size: 10px; text-align:right; margin:6px; margin-top:14px"
-            >
-              {{ item.cardDate }}
-              {{ item.cardTime }}
-            </div>
-        </v-ons-row>
-        <v-ons-row>
-          <div class="content" style="margin-top: 5px">
-            {{ item.text }}
-          </div>
-        </v-ons-row>
-      </v-ons-col>
+<v-ons-list>
+      <v-ons-list-item v-for="item in cardList">
+              <v-ons-col>
+                <v-ons-row>
+                    <div class="title" style="font-size: 20px">
+                      {{ item.title }}
+                    </div>
+                    <div class="fontsmall"
+                      style="font-size: 10px; text-align:right; margin:6px; margin-top:14px"
+                    >
+                      {{ item.cardDate }}
+                      {{ item.cardTime }}
+                    </div>
+                </v-ons-row>
+                <v-ons-row>
+                  <div class="content" style="margin-top: 5px">
+                    {{ item.text }}
+                  </div>
+                </v-ons-row>
+              </v-ons-col>
 
-      <v-ons-col width="30%">
-        <div class="right">
-          <v-ons-button @click="prescription(item.id, cardList.indexOf(item))" modifier="outline large" style="margin:3px"> 확인 </v-ons-button>
-        </div>
-      </v-ons-col>
-    </v-ons-row>
-  </v-ons-card>
+              <v-ons-col width="30%">
+                <div class="right">
+                  <v-ons-button @click="prescription(item.id, cardList.indexOf(item))" modifier="outline large" style="margin:3px"> 확인 </v-ons-button>
+                </div>
+              </v-ons-col>
+      </v-ons-list-item>
+    </v-ons-list>
 
   </v-ons-page>
 </template>
@@ -49,43 +49,19 @@ export default {
           text: "",
           id: "",
         },
-        {
-          type: '',
-          title: "",
-          cardDate: "",
-          cardTime: "",
-          text: "",
-          id: "",
-        },
-        {
-          type: '',
-          title: "",
-          cardDate: "",
-          cardTime: "",
-          text: "",
-          id: "",
-        },
-        {
-          type: '',
-          title: "",
-          cardDate: "",
-          cardTime: "",
-          text: "",
-          id: "",
-        },
-        {
-          type: '',
-          title: "",
-          cardDate: "",
-          cardTime: "",
-          text: "",
-          id: "",
-        },
       ],
       numCard: '',
       reservList: [],
       medicineList: [],
+      count : 0,
     };
+  },
+
+   mounted () {
+    setTimeout(() => {
+      this.count++;
+      this.pushList();
+    }, 1000)
   },
 
   created() {
@@ -137,6 +113,62 @@ export default {
                     this.cardList.splice(idx, 1);
                     this.$emit('updateAlarmBadge', this.cardList.length);
             });
+    },
+       pushList() {
+        //  testing code
+        this.$http.get('http://localhost:3000/api/reservation?id='+  MainPage.patientInfo['id']).then((response) =>     {
+          var cnt = response.data[0].length;
+          var i = 0;
+          for(i = 0; i < cnt; i++) {
+              this.cardList.push( {
+              type: 'reservation',
+              title: "예약",
+              cardDate: "cardDate",
+              cardTime: "cardTime",
+              item: "tiem",
+              });
+          }
+        });
+
+    //      this.$http.get('http://localhost:3000/api/alarm?id='+  MainPage.patientInfo['id']).then((response) => {
+    //     this.reservList = response.data[0];
+    //     this.medicineList = response.data[1];
+
+    //     var reservCnt = this.reservList.length;
+    //     var medicineCnt = this.medicineList.length;
+
+    //     var i = 0;
+
+    //     this.cardList.splice(0, this.cardList.length);
+    //     for(i = 0; i < reservCnt; i++) {
+    //         this.cardList.push( {
+    //             type: 'reservation',
+    //             title: "예약",
+    //             cardDate: this.reservList[i]['resDate'],
+    //             cardTime: this.reservList[i]['resTime'],
+    //             text: "오늘 " + this.reservList[i]['name'] + "에서 " + this.reservList[i]['office'] + " 진료 예약이 있습니다.",
+    //             id: this.reservList[i]['id']
+    //         });
+    //     }
+
+    //     for(i = 0; i < medicineCnt; i++) {
+    //         this.cardList.push( {
+    //             type: 'medicine',
+    //             title: "복약",
+    //             cardDate: this.medicineList[i]['startDate'] + " ~ ",
+    //             cardTime: this.medicineList[i]['endDate'],
+    //             text: this.medicineList[i]['name'] + "을 복용하셨습니까?",
+    //             id: this.medicineList[i]['id']
+    //         });
+    //     }
+        
+    //     this.numCard = this.cardList.length;
+    //     this.$emit('updateAlarmBadge', this.cardList.length);
+    // });
+    setTimeout(() => {
+       this.count++;
+        this.pushList();
+     }, 1000)
     },
     },
 }
